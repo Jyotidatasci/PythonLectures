e2ddcb3f28f91fc1b110b2594b819364519ed3ec
Jyotidatasci <143270056+Jyotidatasci@users.noreply.github.com>
2024-08-07 12:10:22 +0000

    file add

---
+++ Whileloop/Jk1.py
@@ -0,0 +1,18 @@
+#Printing hello 5 times
+count = 0
+while count<5:
+    print("Hello!!")
+    count = count+1
+
+#Printing the numbers in asending order
+count = 0
+while count < 10:
+    count+=1
+    print(count)
+
+#Printing the numbers in reverse order
+count = 15 
+while count >= 0:
+    print(count)
+    count-=1
+    
