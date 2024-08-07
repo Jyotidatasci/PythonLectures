bd14c2a6122e7d3d8416fb0a3222f66852761f16
Jyotidatasci <143270056+Jyotidatasci@users.noreply.github.com>
2024-08-07 12:18:29 +0000

    Add file

---
+++ Whileloop/Jk2.py
@@ -0,0 +1,12 @@
+#Printing the table of 2 using while loop
+count = 0
+stop_at = 20
+while count<= stop_at:
+    print(count)
+    count +=2
+
+#Printing the table of 2 in reverse order using while loop
+count = 20
+while count >=0:
+    print(count)
+    count-=2
